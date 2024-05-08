bdd41d70ec9c93973053084a43bf3d02de0983f2
atitudedeveloper <atitudedeveloper@gmail.com>
2024-05-07 21:36:25 -0300

    ajsutes rodadpe

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -5,7 +5,7 @@ export const Footer = () => {
         <footer className="mt-5">
             <Separator />
             <div className="my-5 text-center text-sm opacity-50">
-                Criado por Fofolete
+                Criado por Maria Lohana
             </div>
         </footer>
     );
